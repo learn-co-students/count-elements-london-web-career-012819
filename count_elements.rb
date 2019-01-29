0b4035443fc41077199386906dd27051d2173a65
Kian Edalat <akedalat@gmail.com>
2019-01-29 20:20:29 +0000

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,13 @@
+require "pry"
 def count_elements(array)
-  # code goes here
+  count_hash = {}
+  array.each do |name|
+    if count_hash.include?(name)
+      count_hash[name] +=1
+    else
+      count_hash[name] = 1
+    end
+  end
+  count_hash
+  #binding.pry
 end
- 
